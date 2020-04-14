e817da635051d11f8422cfe3b20946ab4386e81d
Pawel <matizbrave@gmail.com>
2020-04-14 15:55:17 +0200

    splash screen animation added

---
+++ scripts/splash.js
@@ -0,0 +1,8 @@
+// const splash = document.querySelector('.splash-screen');
+// const hero = document.querySelector('.main');
+
+// window.onload = function(){
+//   splash.style.opacity = '0';
+//   splash.addEventListener('transitionend', () => splash.remove());
+//   hero.style.opacity = '1';
+// };

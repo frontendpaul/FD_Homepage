26ce3112432a38e5d52653d0f7b3478a76f81c88
Pawel <matizbrave@gmail.com>
2020-04-25 23:07:12 +0200

    fixed menu issue

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 const toggler = document.querySelector('.mobile-menu-toggler');
 const mobileMenu = document.querySelector('.mobile-menu-container');
 const bar = document.querySelector('.top-bar-for-mobile-menu');
-const close = document.querySelector('.mobile-menu-close');
-const main = document.querySelector('body');
+const closeBtn = document.querySelector('.mobile-menu-close');
+const links = document.querySelectorAll('.menu-link');
+const main = document.body;
 
 const navigatrorPrefix  = document.querySelector('.navigator-prefix');
 
@@ -12,15 +13,36 @@ toggler.addEventListener('click', function() {
   mobileMenu.style.top = '0';
   bar.style.display = 'flex';
   main.style.overflow = 'hidden';
-  // toggler.classList.toggle('is-activ');
-  // document.querySelector('.main-menu').classList.toggle('mobile-menu-visible');
 });
 
-close.addEventListener('click', function() {
+const open = function() {
+  mobileMenu.style.top = '0';
+  bar.style.display = 'flex';
+  main.style.overflow = 'hidden';
+}
+
+const close = function() {
   mobileMenu.style.top = '-100vh';  
   main.style.overflow = 'visible';
   setTimeout(function(){ bar.style.display = 'none'; }, 300);
+}
+
+toggler.addEventListener('click', e => {
+  open();
+  e.preventDefault();  
 });
+
+closeBtn.addEventListener('click', e => {
+  close();
+  e.preventDefault();  
+});
+
+for(const link of links) {
+  link.addEventListener('click', e => {
+    close();
+  });
+}
+
 
 if (window.screen.width >=760) {
   navigatrorPrefix.innerHTML = 'page';
